aa8ab327c907266d103aa4722e0d404f369c3f43
Michael Haber <michaeladam.haber@gmail.com>
2020-07-02 16:34:59 -0400

    Finished generating my random password

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Create my variables for the user confirmations.
-var length = "";
+
+var passwordLength = "";
 var yesNumber;
 var yesCharacter;
 var yesUpper;
 var yesLower;
 // Set my arrays up
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+console.log(numbers);
 var lowers = [
   "a",
   "b",
@@ -34,6 +36,7 @@ var lowers = [
   "y",
   "z",
 ];
+console.log(lowers);
 var uppers = [
   "A",
   "B",
@@ -62,9 +65,13 @@ var uppers = [
   "Y",
   "Z",
 ];
+console.log(uppers);
 var characters = ["!", "#", "$", "%", "&", "@"];
+console.log(characters);
 // What if the user says no to all of the prompts?
 var mustChoose;
+// Create a variable that stores all the user's parameters
+var userSelections;
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -72,20 +79,20 @@ var generateBtn = document.querySelector("#generate");
 // Start creating my function
 function generatePassword() {
   // User chooses how many characters in their password
-  var length = prompt(
+  var passwordLength = prompt(
     "How many characters would you like your password to be? Aim for a number between 8 and 128!"
   );
   // Create a loop just in case user inputs number outside of parameters or hits the cancel button
-  while (length < 8 || length > 128) {
+  while (passwordLength < 8 || passwordLength > 128) {
     alert(
       "Your password must be between 8 and 128 characters. Please try again!"
     );
-    var length = prompt(
+    var passwordLength = prompt(
       "How many characters would you like your password to be? Aim for a number between 8 and 128!"
     );
   }
   // Tell the user how many characters their password will be
-  alert("Your password will have " + length + " characters");
+  alert("Your password will have " + passwordLength + " characters");
 
   // Ask the user to confirm the other parameters
   var yesNumber = confirm(
@@ -108,6 +115,7 @@ function generatePassword() {
     yesUpper !== true
   ) {
     alert("You must select at least one parameter!");
+    // Confirm which characters the user wants in their password
     var yesNumber = confirm(
       "Click OK if you would like your password to include a number."
     );
@@ -121,7 +129,39 @@ function generatePassword() {
       "Click OK if you would like your password to include an uppercase letter."
     );
   }
+  //  Create an empty set array of all the characters that COULD be selected
+  var passwordCharacters = [];
+  //  Concatenate all the possible characters so that they are all in the string
+  if (yesNumber === true) {
+    passwordCharacters = passwordCharacters.concat(numbers);
+  }
+
+  if (yesCharacter === true) {
+    passwordCharacters = passwordCharacters.concat(characters);
+  }
+
+  if (yesLower === true) {
+    passwordCharacters = passwordCharacters.concat(lowers);
+  }
+
+  if (yesUpper === true) {
+    passwordCharacters = passwordCharacters.concat(uppers);
+  }
+  // Test this in the console log to see if your pool of possible password characters shows up correctly
+  console.log(passwordCharacters);
+  // Create empty set of final password
+  var finalPassword = "";
+  // Create a loop that continues to pull random characters until it reaches the user's length choice
+  for (var i = 0; i < passwordLength; i++) {
+    // Final password should pull from possible characters but use user's choice for length
+    finalPassword +=
+      passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    console.log(finalPassword);
+  }
+  // Return final password in the textbox
+  return finalPassword;
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
